380429ac31505743e17f9af29575bd31ff4d6606
Jack Greiner <jack@emoss.org>
2024-05-04 03:56:35 -0400

    Make sure javascript isn't deployed accidentally.
    Additionally connect everything to site.js.

--- content/site.js
+++ content/site.js
@@ -2,6 +2,7 @@
 //= require js/_bootstrap.js
 //= require js/_debounce.js
 //= require js/_fluidbox.js
+//= require js/_nebula.js
 
 $(document).ready( function() {
    $(".use-fluidbox").fluidbox();
