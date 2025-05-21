c19b4e42938a38ef116d11ceb3f563106ee7e9e3
Jack Greiner <jack@emoss.org>
2025-05-21 16:01:09 -0400

    Adjust sitemap generation to avoid adding the sitemap itself (or any feeds) to it.

--- content/sitemap.rb
+++ content/sitemap.rb
@@ -1,6 +1,8 @@
-<%= xml_sitemap :items => @items.reject{ |i|
-      i[:is_hidden] ||
-      (i.binary? and not i[:extension] == 'pdf') ||
-      i.identifier.to_s.match( /.(js|sass|scss|css)$/ ) ||
-      i.identifier == '/sitemap.xml'
-} %>
+<%= xml_sitemap \
+  :items => @items.reject { |i|
+    i[:is_hidden] ||
+    (i.binary? && i[:extension] != 'pdf') ||
+    i.identifier.to_s =~ /\.(js|sass|scss|css|xml)$/ ||
+    i.identifier.to_s =~ %r{^/sitemap}
+  }
+%>
